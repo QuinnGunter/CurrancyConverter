5d13bac5ceeb6dcd0c95a66d3e459fd1cefc91c4
Quinn <quinngunter.qg@gmail.com>
2017-02-02 13:54:54 -0700

    Add a comment stating what line of code is the Clear Button I created

--- CurrancyConverter/ViewController.swift
+++ CurrancyConverter/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         inputTextField.clearButtonMode = .always
     }
     
+    //Clear Button
     @IBAction func clearAmount(_ sender: UIButton){
         inputTextField.text = ""
         convertCurrency(sender)
